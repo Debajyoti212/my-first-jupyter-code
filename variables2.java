c4c368b735ac52cda60cc93aec930d1038d7334b
Debajyoti Majumder <debajyoti344@gmail.com>
2025-04-23 23:17:29 +0530

    Delete variables2.java

--- variables2.java
+++
@@ -1,8 +0,0 @@
-public class variables2 {
-   public static void main(String[]args) {
-     int count=12;
-     byte marks=13;
-     System.out.println(count);
-     System.out.println(marks);
-   }
-}
