11a1cb6735edca15fbbef0b3d56b04ecf1e2be77
Debajyoti Majumder <debajyoti344@gmail.com>
2025-04-23 21:42:07 +0530

    Add files via upload

---
+++ variables2.java
@@ -0,0 +1,8 @@
+public class variables2 {
+   public static void main(String[]args) {
+     int count=12;
+     byte marks=13;
+     System.out.println(count);
+     System.out.println(marks);
+   }
+}
